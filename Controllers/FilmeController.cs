190909f1c90455e6ccd0b31be88e54320cdde519
Diego Niles <diego.niles@hotmail.com>
2023-08-19 11:24:02 -0300

    Adicionado comportamento básico de um request do tipo Post

---
+++ Controllers/FilmeController.cs
@@ -0,0 +1,19 @@
+﻿using FilmesApiRest.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FilmesApiRest.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class FilmeController : ControllerBase
+{
+    private static List<Filme> filmes = new List<Filme>();
+
+    [HttpPost]
+    public void AdicionaFilme([FromBody] Filme filme)
+    {
+        filmes.Add(filme);
+        Console.WriteLine(filme.Titulo);
+        Console.WriteLine(filme.Duracao);
+    }
+}
---
+++ Models/Filme.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace FilmesApiRest.Models;
+
+public class Filme
+{
+    [Required(ErrorMessage = "O título do filme é obrigatório!!!")]
+    [MaxLength(100, ErrorMessage = "O tamanho máximo do título são 100 caracteres")]
+    public string Titulo { get; set; }
+
+    [Required(ErrorMessage = "O gênero do filme é obrigatório!!!") ]
+    [MaxLength(50, ErrorMessage = "O tamanho máximo do genêro são 50 caracteres.")]
+    public string  Genero { get; set; }
+
+    [Required]
+    [MaxLength(50, ErrorMessage = "O tamanho máximo do nome do diretor são 50 caracteres.")]
+    public string Diretor { get; set; }
+
+    [Required]
+    [Range(70, 600, ErrorMessage = "A duração do filme deve ter entre 70 e 600 minutos.")]
+    public int Duracao { get; set; }             
+}
