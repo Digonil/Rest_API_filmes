a7e558bee3ef8e305952752569dd952f0c721e60
Diego Niles <diego.niles@hotmail.com>
2023-08-19 20:36:40 -0300

    Criado o FilmeContext e as configurações do banco de dados

---
+++ Data/FilmeContext.cs
@@ -0,0 +1,15 @@
+﻿using FilmesApiRest.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace FilmesApiRest.Data;
+
+public class FilmeContext : DbContext
+{
+    public FilmeContext(DbContextOptions<FilmeContext> opts)
+        : base(opts)
+    {
+            
+    }
+
+    public DbSet<Filme> filmes { get; set; }
+}
--- Program.cs
+++ Program.cs
@@ -1,4 +1,14 @@
+using FilmesApiRest.Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
+
+//Pega os dados de conexão no appsettings.json para criar o connectionstring
+var connectionString = builder.Configuration.GetConnectionString("FilmeConnection");
+
+//Add context of DB connection
+builder.Services.AddDbContext<FilmeContext>(opts => 
+    opts.UseMySql(connectionString, ServerVersion.AutoDetect(connectionString)));
 
 // Add services to the container.
 
