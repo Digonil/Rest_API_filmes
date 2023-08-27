50664d8202a9c858e18a9b71320708b3899746ba
Diego Niles <diego.niles@hotmail.com>
2023-08-27 17:39:12 -0300

    Criado consulta com Linq no controller de Filme

--- Controllers/FilmeController.cs
+++ Controllers/FilmeController.cs
@@ -38,10 +38,16 @@ public class FilmeController : ControllerBase
     }
 
     [HttpGet]
-    public IEnumerable<ReadFilmeDto> RecuperaFilmes([FromQuery] int skip=0, [FromQuery] int take=20)
+    public IEnumerable<ReadFilmeDto> RecuperaFilmes([FromQuery] int skip=0, [FromQuery] int take=20, [FromQuery] string? nomeCinema = null)
     {
-        return _mapper.Map<List<ReadFilmeDto>>(_context.filmes.Skip(skip).Take(take).ToList());
+        if(nomeCinema == null)
+        {
+            return _mapper.Map<List<ReadFilmeDto>>(_context.filmes.Skip(skip).Take(take).ToList());
+        }
+
+        return _mapper.Map<List<ReadFilmeDto>>(_context.filmes.Skip(skip).Take(take).Where(filme => filme.Sessoes.Any(sessao => sessao.Cinema.Nome == nomeCinema)).ToList());
     }
+
 
     [HttpGet("{id}")]
     public IActionResult RecuperaFimePorId(int id) 
